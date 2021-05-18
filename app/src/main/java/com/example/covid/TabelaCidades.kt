30bf7eac7d3d01f45f97a04f02f75dc8e66b765d
João Santos <joaoo.santoss.98@gmail.com>
2021-05-18 20:28:43 +0100

    criada as colunas da Tabela Cidades

--- app/src/main/java/com/example/covid/TabelaCidades.kt
+++ app/src/main/java/com/example/covid/TabelaCidades.kt
@@ -1,12 +1,13 @@
 package com.example.covid
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
 class TabelaCidades(db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
 
     fun cria() {
-        db.execSQL("")
+        db.execSQL("CREATE TABLE cidades (_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL )")
     }
 
 }
