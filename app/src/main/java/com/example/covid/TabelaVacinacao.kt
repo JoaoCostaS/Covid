43bdf5cf72e822fee15b1a30a96170934a09e864
João Santos <joaoo.santoss.98@gmail.com>
2021-05-19 15:55:13 +0100

    Associadas constantes para o nome da Tabela Vacinacao e dos campos

--- app/src/main/java/com/example/covid/TabelaVacinacao.kt
+++ app/src/main/java/com/example/covid/TabelaVacinacao.kt
@@ -7,6 +7,15 @@ class TabelaVacinacao(db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
 
     fun cria() {
-        db.execSQL("CREATE TABLE vacinacao(id INTEGER PRIMARY KEY AUTOINCREMENT, vacinados INTEGER, naovacinados INTEGER)"
+        db.execSQL("CREATE TABLE " + NOME_TABELA + "(" +
+                BaseColumns._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+                CAMPO_VACINADOS + "INTEGER, " +
+                CAMPO_NAOVACINADOS + "INTEGER" +
+                ")")
+    }
+    companion object{
+        const val NOME_TABELA = "vacinacao"
+        const val CAMPO_VACINADOS = "vacinados"
+        const val CAMPO_NAOVACINADOS = "naovacinados"
     }
 }
